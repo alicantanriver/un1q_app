1b3a2dd51ba747674be6ba0d6d13b7850c5a5158
Alican Tanriver <alicantanriver@gmail.com>
2023-12-14 13:25:29 +0300

    adding test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:un1q_app/views/categories_view.dart';
 
+void main() {
+  testWidgets("Test Category View Page", (tester) async {
+    await tester.runAsync(() async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: CategoriesView(),
+        ),
+      );
+      expect(find.byType(CategoryList, skipOffstage: false), findsOneWidget);
+    });
+  });
+}
